711557840f0a53d148f389fa0691973acc68b4a0
ahmad's MacBook Air <akarimqa@gmail.com>
2016-04-10 14:47:06 -0400

    exam fina1

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -12,7 +12,7 @@ public class DuplicateWord {
          * Write a java program to find the duplicate words and their number of occurrences in the string.
          * Also Find the average length of the words.
          */
-
+/*
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
 
 
@@ -47,3 +47,4 @@ public class DuplicateWord {
     }
 
 }
+/*
